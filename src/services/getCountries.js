abe55526a81cc7d9a90ed40f98ffea4cb9c2a476
Kenni Woodard <kennikristine@gmail.com>
2023-01-18 19:22:24 -0800

    linting error

--- src/services/getCountries.js
+++ src/services/getCountries.js
@@ -2,6 +2,5 @@ import { checkError, client } from './client.js';
 
 export async function getCountries() {
   const response = await client.from('countries').select('*');
-  console.log(response, 'response');
   return checkError(response);
 }
